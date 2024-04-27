f8ba6257b36c95a64acda595e43968417b04de8d
rayen-ben-masseoud <Rayen.BEN-MASSEOUD@student.isae-supaero.fr>
2024-04-27 23:12:46 +0200

    superuser

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -15,12 +15,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&cwgh@oq-)vmx*nu2*z7of#x#e^i(mh-e%81rfc#17a*vm^ty='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
