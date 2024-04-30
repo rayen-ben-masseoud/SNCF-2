9944eb24e97d98d7d728f6ccbc3ac874b0b19433
rayen-ben-masseoud <Rayen.BEN-MASSEOUD@student.isae-supaero.fr>
2024-04-30 22:19:04 +0200

    fixer settings

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -15,10 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
